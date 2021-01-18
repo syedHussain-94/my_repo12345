8a0c92c5ddacce60ea7bdd85c1b3d8cd8761d037
syedHussain-94 <59933720+syedHussain-94@users.noreply.github.com>
2021-01-18 12:45:09 -0700

    Commited via Dolly

---
+++ tests/test_my_repo12345.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+test_my_repo12345
+----------------------------------
+Tests for 'my_repo12345' module.
+"""
+
+import unittest
+
+from my_repo12345 import my_repo12345
+
+
+class TestMy_repo12345(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
